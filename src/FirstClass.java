b9d56456e72d018d440e8988cb3e52da61ed45b7
DmitriyStasenko <kpi22526@gmail.com>
2016-10-27 12:24:47 +0300

    Initial commit

---
+++ src/FirstClass.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Дима on 27.10.2016.
+ */
+public class FirstClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("Мой первый класс на Java");
+
+
+
+    }
+
+
+}
---
+++ src/ThirdClass.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Дима on 27.10.2016.
+ */
+public class ThirdClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("Мой первый класс на Java");
+
+
+    }
+
+
+}
